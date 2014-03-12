89104a47a0ef74bb3a95b5497bd154da63231394
Zakaria <zakameziane@hotmail.com>
2014-03-12 16:23:38 +0100

    updated build.scala

--- project/Build.scala
+++ project/Build.scala
@@ -1,3 +1,21 @@
-val appDependencies = Seq(
-  "postgresql" % "postgresql" % "8.4-702.jdbc4"
-)
+import sbt._
+import Keys._
+import PlayProject._
+
+object ApplicationBuild extends Build {
+
+  val appName         = "Your application"
+  val appVersion      = "1.0"
+
+  val appDependencies = Seq(
+    // Add your project dependencies here,
+    "postgresql" % "postgresql" % "8.4-702.jdbc4"
+  )
+
+  val main = PlayProject(
+    appName, appVersion, appDependencies, mainLang = SCALA
+  ).settings(
+    // Add your own project settings here      
+  )
+
+}
